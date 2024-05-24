fbe133bbfa84ac4cf522b6170d19515954d35eec
MSRossii <marcelosrossi13@gmail.com>
2024-05-24 02:24:26 -0300

    add removeBtn

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,10 @@ export default function App() {
     setGames((state) => [...state, game]);
   };
 
+  const removeGame = (id) => {
+    setGames((state) => state.filter((game) => game.id !== id));
+  };
+
   const handleSubmit = (ev) => {
     ev.preventDefault();
     addGame({ title, cover });
@@ -50,7 +54,7 @@ export default function App() {
             <img src={game.cover} alt="" />
             <div>
               <h2>{game.title}</h2>
-              <button>Remover</button>
+              <button onClick={() => removeGame(game.id)}>Remover</button>
             </div>
           </div>
         ))}
