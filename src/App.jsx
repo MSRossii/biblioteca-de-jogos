598a6bc4ccbd00783defe1be84d008a0a72d928e
MSRossii <marcelosrossi13@gmail.com>
2024-05-24 02:06:07 -0300

    inputs

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,30 @@ export default function App() {
   const handleSubmit = (ev) => {
     ev.preventDault();
   };
+
   return (
     <div id="app">
       <h1>Biblioteca de Jogos</h1>
       <form>
         <div>
           <label htmlFor="title">Título: </label>
-          <input type="text" name="title" id="title" />
+          <input
+            type="text"
+            name="title"
+            id="title"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="cover">Capa: </label>
-          <input type="text" name="cover" id="cover" />
+          <input
+            type="text"
+            name="cover"
+            id="cover"
+            value={cover}
+            onChange={(e) => setCover(e.target.value)}
+          />
         </div>
         <button type="submit">Adicionar à biblioteca</button>
       </form>
