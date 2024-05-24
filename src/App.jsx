9c87649688df011e8c0cd00791f746a3b1f17c5e
MSRossii <marcelosrossi13@gmail.com>
2024-05-24 02:47:48 -0300

    last changes tonight

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from "react";
 
-export default function App() {
+function App() {
   const [games, setGames] = useState(() => {
-    const storedGames = localStorage.getItem("obg-game-lib");
+    const storedGames = localStorage.getItem("obc-game-lib");
     if (!storedGames) return [];
     return JSON.parse(storedGames);
   });
@@ -11,7 +11,7 @@ export default function App() {
   const [cover, setCover] = useState("");
 
   const addGame = ({ title, cover }) => {
-    const id = Math.floor(Math.random() * 100000);
+    const id = Math.floor(Math.random() * 1000000);
     const game = { id, title, cover };
     setGames((state) => {
       const newState = [...state, game];
@@ -36,35 +36,33 @@ export default function App() {
   };
 
   return (
-    <div id="app">
+    <div className="app">
       <h1>Biblioteca de Jogos</h1>
       <form onSubmit={handleSubmit}>
         <div>
-          <label htmlFor="title">Título: </label>
+          <label htmlFor="title">Título:</label>
           <input
             type="text"
-            name="title"
             id="title"
             value={title}
-            onChange={(e) => setTitle(e.target.value)}
+            onChange={(ev) => setTitle(ev.target.value)}
           />
         </div>
         <div>
-          <label htmlFor="cover">Capa: </label>
+          <label htmlFor="cover">Capa:</label>
           <input
             type="text"
-            name="cover"
             id="cover"
             value={cover}
-            onChange={(e) => setCover(e.target.value)}
+            onChange={(ev) => setCover(ev.target.value)}
           />
         </div>
-        <button type="submit">Adicionar à biblioteca</button>
+        <button>Adicionar</button>
       </form>
       <div className="games">
         {games.map((game) => (
           <div key={game.id}>
-            <img src={game.cover} alt="" />
+            <img src={game.cover} alt="Capa do jogo" />
             <div>
               <h2>{game.title}</h2>
               <button onClick={() => removeGame(game.id)}>Remover</button>
@@ -75,3 +73,5 @@ export default function App() {
     </div>
   );
 }
+
+export default App;
