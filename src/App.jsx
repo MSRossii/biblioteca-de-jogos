9f32a5c8e92f723a252af66d8800ec37441ba1aa
MSRossii <marcelosrossi13@gmail.com>
2024-05-24 02:33:16 -0300

    create localStorage ein addGame and removeGame

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,18 @@ export default function App() {
   const addGame = ({ title, cover }) => {
     const id = Math.floor(Math.random() * 100000);
     const game = { id, title, cover };
-    setGames((state) => [...state, game]);
+    setGames((state) => {
+      const newState = [...state, game];
+      localStorage.setItem("obc-game-lib", JSON.stringify(newState));
+      return newState;
+    });
   };
-
   const removeGame = (id) => {
-    setGames((state) => state.filter((game) => game.id !== id));
+    setGames((state) => {
+      const newState = state.filter((game) => game.id !== id);
+      localStorage.setItem("obc-game-lib", JSON.stringify(newState));
+      return newState;
+    });
   };
 
   const handleSubmit = (ev) => {
